aa1f9df8053de525bf204cedce5507a4065e03cc
cuong <cuong.nguyen@crossian.com>
2025-02-28 14:05:06 +0700

    minor change

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -61,7 +61,7 @@ const socialLinks = [
     </g>
 </svg>
     ),
-    url: "https://tiktok.com/@eki_zulfar",
+    url: "https://tiktok.com/@shawndevsetup",
     color: "black",
     gradient: "from-[#000000] via-[#25F4EE] to-[#FE2C55]"
   }
