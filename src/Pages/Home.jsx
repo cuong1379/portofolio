fe705fa172e87b8bcb4a66bfa52b66adc6b53470
cuong <cuong.nguyen@crossian.com>
2025-03-01 14:18:12 +0700

    minor enhancess

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -77,8 +77,8 @@ const SocialLink = memo(({ icon: Icon, link }) => (
 const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
-const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
-const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
+const WORDS = ["Network & E-commerce", "Technical Architecture", "Tech Enthusiast", ];
+const TECH_STACK = ["React", "Node.js", "Kubernetes", "AWS"];
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/cuong1379" },
   { icon: Linkedin, link: "https://www.linkedin.com/in/nguyen-cuong-825a98160/" },
